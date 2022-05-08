8fe4961246624f743a00736e4dd1c69afdf96401
sklim84 <captiong84@gmail.com>
2022-05-08 21:03:42 +0900

    10회 평균 accuracy 실험

--- midterm_report/mlp.py
+++ midterm_report/mlp.py
@@ -11,17 +11,21 @@ import pandas as pd
 # 4. What is the optimal network structure? How is the 'optimality' defined?
 ####################
 
-# 학습/테스트 데이터 생성
+# train/test 데이터 생성
 train_data, train_label, test_data, test_label = data_loader.load()
 print(train_data.shape)
 print(train_label.shape)
 
+# 졍확도 평가를 위한 모델별 반복 train/test 횟수
+num_iter = 10
+
+####################
 # RQ 1. structure
-# num of layers, num of neurons
+# - num of layers, num of neurons
+####################
 hidden_layers = [3, 4, 5]
 hidden_neurons = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-
-accuracy_list = []
+arch_accr_list = []
 for layer in hidden_layers:
     for neuron in hidden_neurons:
         mlp_model = MLPClassifier(hidden_layer_sizes=(layer, neuron),
@@ -29,22 +33,47 @@ for layer in hidden_layers:
                                   solver="adam",
                                   early_stopping=True,
                                   max_iter=200)
-        mlp_model.fit(train_data, train_label)
-        # pred_label = mlp_model.predict(test_data)
-        mean_accucacy = mlp_model.score(test_data, test_label)
-        print('hidden layrs: {}, hidden neurons: {}, mean accuracy: {}'.format(layer, neuron, mean_accucacy))
-        accuracy_list.append((layer, neuron, mean_accucacy))
+        mean_accr = 0
+        for index in range(num_iter + 1):
+            mlp_model.fit(train_data, train_label)
+            accr = mlp_model.score(test_data, test_label)
+            print('hidden layrs: {}, hidden neurons: {}, accuracy: {}'.format(layer, neuron, accr))
+            mean_accr += accr / num_iter
+        arch_accr_list.append((layer, neuron, mean_accr))
 
-df_accuracy = pd.DataFrame(accuracy_list, columns=['hidden layers', 'hidden neurons', 'mean accuracy'])
-print(df_accuracy)
-df_accuracy.to_csv('./results/mlp_accuracy.csv', index=False)
+df_arch_accr = pd.DataFrame(arch_accr_list, columns=['hidden layers', 'hidden neurons', 'mean accuracy'])
+print(df_arch_accr)
+df_arch_accr.to_csv('./results/mlp_arch_accr.csv', index=False)
 
+####################
 # RQ 2. activation function (layers: 5, neurons: 256)
 # - logistic
 # - tanh
 # - relu(def)
+####################
+activation_function = ['logistic', 'tanh', 'relu']
+num_layers, num_neurons = 5, 256
+act_func_accr_list = []
+for act_func in activation_function:
+    mlp_model = MLPClassifier(hidden_layer_sizes=(num_layers, num_neurons),
+                              activation=act_func,
+                              solver="adam",
+                              early_stopping=True,
+                              max_iter=200)
+    mean_accr = 0
+    for index in range(num_iter + 1):
+        mlp_model.fit(train_data, train_label)
+        accr = mlp_model.score(test_data, test_label)
+        print('activation function: {}, accuracy: {}'.format(act_func, accr))
+        mean_accr += accr / num_iter
+    act_func_accr_list.append((act_func, mean_accr))
 
-# RQ 3. learning algorithm (layers: , neurons: )
+df_acr_func_accr = pd.DataFrame(act_func_accr_list, columns=['activation function', 'mean accuracy'])
+print(df_acr_func_accr)
+df_acr_func_accr.to_csv('./results/mlp_act_func_accr.csv', index=False)
+
+####################
+# RQ 3. learning algorithm (layers: 5, neurons: 256)
 # - lbfgs : the family of quasi-Newton methods
 # - sgd : stochastic gradient descent
 #   - learning_rate(def=constant, invscaling, adaptive), learning_rate_init(def=0.001)
@@ -52,5 +81,9 @@ df_accuracy.to_csv('./results/mlp_accuracy.csv', index=False)
 # - adam
 #   - learning_rate_init(def=0.001), early_stopping(def=False)
 #   - beta_1(def=0.9), beta_2(def=0.999), epsilon(def=1e-8)
+####################
 
+
+####################
 # RQ 4. optimal network structure
+####################
