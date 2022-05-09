a39d4a115ec987101b8339e4ee650af52f54ab0c
sklim84 <captiong84@gmail.com>
2022-05-09 17:52:54 +0900

    MLP architecture test 수정(layer/neuron)

--- midterm_report/mlp.py
+++ midterm_report/mlp.py
@@ -9,7 +9,6 @@ import time
 # 1. How does the choice of the structure influence the performance?
 # 2. How does the choice of the activation function influence the performance?
 # 3. How does the choice of the learning algorithm influence the performance?
-# 4. What is the optimal network structure? How is the 'optimality' defined?
 ####################
 
 # train/test 데이터 생성
@@ -24,25 +23,27 @@ num_iter = 10
 # RQ 1. structure
 # - num of layers, num of neurons
 ####################
-hidden_layers = [3, 4, 5]
-hidden_neurons = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+hidden_layers = [1, 2, 3]
+hidden_neurons = [2, 4, 8, 16, 32, 64, 128, 256]
 arch_accr_list = []
 for layer in hidden_layers:
     for neuron in hidden_neurons:
-        mlp_model = MLPClassifier(hidden_layer_sizes=(layer, neuron),
+        hidden_layer_sizes = (neuron, ) * layer
+        mlp_model = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes,
                                   activation='relu',
                                   solver="adam",
                                   early_stopping=True,
-                                  max_iter=200)
+                                  max_iter=100)
+
         mean_accr = 0
         mean_elapsed_time = 0
         for index in range(num_iter):
             start_time = time.time()
-            mlp_model.fit(train_data, train_label)
+            mlp_model = mlp_model.fit(train_data, train_label)
             accr = mlp_model.score(test_data, test_label)
             elapsed_time = time.time() - start_time
             print('hidden layrs: {}, hidden neurons: {}, accuracy: {}, elapsed time: {}'.format(layer, neuron, accr,
-                                                                                                 elapsed_time))
+                                                                                                elapsed_time))
             mean_accr += accr / num_iter
             mean_elapsed_time += elapsed_time / num_iter
         arch_accr_list.append((layer, neuron, mean_accr, mean_elapsed_time))
@@ -54,13 +55,36 @@ print(df_arch_accr)
 df_arch_accr.to_csv('./results/mlp_arch_accr.csv', index=False)
 
 ####################
-# RQ 2. activation function (layers: 5, neurons: 512)
+# RQ 1-1. epoch (layers: 2, neurons: 64)
+####################
+num_layers, num_neurons, num_epoch = 2, 64, 200
+epoch_accr_list = []
+for epoch in range(num_epoch):
+    mlp_model = MLPClassifier(hidden_layer_sizes=(num_layers, num_neurons),
+                              activation='relu',
+                              solver="adam",
+                              early_stopping=True,
+                              max_iter=epoch + 1)
+
+    mlp_model = mlp_model.fit(train_data, train_label)
+    accr = mlp_model.score(test_data, test_label)
+    print('epoch: {}, accuracy: {}'.format(epoch + 1, accr))
+
+    epoch_accr_list.append((epoch + 1, accr))
+
+# epoch별 accuracy 저장
+df_epoch_accr = pd.DataFrame(epoch_accr_list, columns=['epoch', 'accuracy'])
+print(df_epoch_accr)
+df_epoch_accr.to_csv('./results/mlp_epoch_accr_{}_{}_{}.csv'.format(num_epoch, num_layers, num_neurons), index=False)
+
+####################
+# RQ 2. activation function (layers: 2, neurons: 64)
 # - logistic
 # - tanh
 # - relu(def)
 ####################
 activation_function = ['logistic', 'tanh', 'relu']
-num_layers, num_neurons = 5, 512
+num_layers, num_neurons = 2, 64
 act_func_accr_list = []
 for act_func in activation_function:
     mlp_model = MLPClassifier(hidden_layer_sizes=(num_layers, num_neurons),
@@ -70,7 +94,7 @@ for act_func in activation_function:
                               max_iter=200)
     mean_accr = 0
     for index in range(num_iter):
-        mlp_model.fit(train_data, train_label)
+        mlp_model = mlp_model.fit(train_data, train_label)
         accr = mlp_model.score(test_data, test_label)
         print('activation function: {}, accuracy: {}'.format(act_func, accr))
         mean_accr += accr / num_iter
@@ -78,10 +102,11 @@ for act_func in activation_function:
 
 df_acr_func_accr = pd.DataFrame(act_func_accr_list, columns=['activation function', 'mean accuracy'])
 print(df_acr_func_accr)
-df_acr_func_accr.to_csv('./results/mlp_act_func_accr.csv', index=False)
+df_acr_func_accr.to_csv('./results/mlp_act_func_accr_{}_{}.csv'.format(num_layers, num_neurons), index=False)
+# TODO epoch 증가에 따른 activation function별 accuracy 추이
 
 ####################
-# RQ 3. learning algorithm (layers: 5, neurons: 512)
+# RQ 3. learning algorithm (layers: 5, neurons: 128)
 # - lbfgs : the family of quasi-Newton methods
 # - sgd : stochastic gradient descent
 #   - learning_rate(def=constant, invscaling, adaptive), learning_rate_init(def=0.001)
@@ -90,8 +115,4 @@ df_acr_func_accr.to_csv('./results/mlp_act_func_accr.csv', index=False)
 #   - learning_rate_init(def=0.001), early_stopping(def=False)
 #   - beta_1(def=0.9), beta_2(def=0.999), epsilon(def=1e-8)
 ####################
-
-
-####################
-# RQ 4. optimal network structure
-####################
+# TODO epoch 증가에 따른 learning algorithm별 accuracy 추이
