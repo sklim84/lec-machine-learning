1ead836d136996c004479388f1334a6cc4b241d5
sklim84 <captiong84@gmail.com>
2022-05-10 17:31:49 +0900

    주석 정리

--- midterm_report/visualize.py
+++ midterm_report/visualize.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 # ###################
 # RQ 1-1. epoch (layers: 2, neurons: 64)
 # ###################
-# df_mlp_epoch_accr = pd.read_csv('./results/mlp_epoch_accr_300_2_64.csv')
-# print(df_mlp_epoch_accr)
-#
-# mean_accr_list = df_mlp_epoch_accr['mean accuracy'].to_list()
-# epochs = list(range(1, 301))
-#
-# plt.title('2 hidden layers and 64 neurons')
-# plt.xlabel('epochs')
-# plt.xticks([1, 100, 200, 300])
-# plt.ylabel('Test Accuracy')
-# plt.ylim(0, 1.0)
-# plt.plot(epochs, mean_accr_list)
-# plt.show()
+df_mlp_epoch_accr = pd.read_csv('./results/mlp_epoch_accr_300_2_64.csv')
+print(df_mlp_epoch_accr)
+
+mean_accr_list = df_mlp_epoch_accr['mean accuracy'].to_list()
+epochs = list(range(1, 301))
+
+plt.title('2 hidden layers and 64 neurons')
+plt.xlabel('epochs')
+plt.xticks([1, 100, 200, 300])
+plt.ylabel('Test Accuracy')
+plt.ylim(0, 1.0)
+plt.plot(epochs, mean_accr_list)
+plt.show()
 
 
 ####################
@@ -25,23 +25,23 @@ import matplotlib.pyplot as plt
 # - tanh
 # - relu(def)
 ####################
-# df_act_func_accr = pd.read_csv('./results/mlp_act_func_accr_2_64.csv')
-# print(df_act_func_accr)
-#
-# sigmoid_accr_list = df_act_func_accr.iloc[:100, :]['mean accuracy'].to_list()
-# tanh_accr_list = df_act_func_accr.iloc[100:200, :]['mean accuracy'].to_list()
-# relu_accr_list = df_act_func_accr.iloc[200:, :]['mean accuracy'].to_list()
-# epochs = list(range(1, 101))
-#
-# plt.xlabel('epochs')
-# plt.xticks([1, 25, 50, 75, 100])
-# plt.ylabel('Test Accuracy')
-# plt.ylim(0, 1.0)
-# plt.plot(epochs, sigmoid_accr_list, label='sigmoid')
-# plt.plot(epochs, tanh_accr_list, label='tanh')
-# plt.plot(epochs, relu_accr_list, label='relu')
-# plt.legend(loc='lower right')
-# plt.show()
+df_act_func_accr = pd.read_csv('./results/mlp_act_func_accr_2_64.csv')
+print(df_act_func_accr)
+
+sigmoid_accr_list = df_act_func_accr.iloc[:100, :]['mean accuracy'].to_list()
+tanh_accr_list = df_act_func_accr.iloc[100:200, :]['mean accuracy'].to_list()
+relu_accr_list = df_act_func_accr.iloc[200:, :]['mean accuracy'].to_list()
+epochs = list(range(1, 101))
+
+plt.xlabel('epochs')
+plt.xticks([1, 25, 50, 75, 100])
+plt.ylabel('Test Accuracy')
+plt.ylim(0, 1.0)
+plt.plot(epochs, sigmoid_accr_list, label='sigmoid')
+plt.plot(epochs, tanh_accr_list, label='tanh')
+plt.plot(epochs, relu_accr_list, label='relu')
+plt.legend(loc='lower right')
+plt.show()
 
 
 ####################
@@ -54,45 +54,47 @@ import matplotlib.pyplot as plt
 #   - learning_rate_init(def=0.001), early_stopping(def=False)
 #   - beta_1(def=0.9), beta_2(def=0.999), epsilon(def=1e-8)
 ####################
-# df_lrn_alg_accr = pd.read_csv('./results/mlp_lrn_alg_accr_2_64.csv')
-# print(df_lrn_alg_accr)
-#
-# lbfgs_accr_list = df_lrn_alg_accr.iloc[:100, :]['mean accuracy'].to_list()
-# nesterovs_accr_list = df_lrn_alg_accr.iloc[100:200, :]['mean accuracy'].to_list()
-# adam_accr_list = df_lrn_alg_accr.iloc[200:, :]['mean accuracy'].to_list()
-# epochs = list(range(1, 101))
-#
-# plt.xlabel('epochs')
-# plt.xticks([1, 25, 50, 75, 100])
-# plt.ylabel('Test Accuracy')
-# plt.ylim(0, 1.0)
-# plt.plot(epochs, lbfgs_accr_list, label='lbfgs')
-# plt.plot(epochs, nesterovs_accr_list, label='sgd')
-# plt.plot(epochs, adam_accr_list, label='adam')
-# plt.legend(loc='lower right')
-# plt.show()
+df_lrn_alg_accr = pd.read_csv('./results/mlp_lrn_alg_accr_2_64.csv')
+print(df_lrn_alg_accr)
+
+lbfgs_accr_list = df_lrn_alg_accr.iloc[:100, :]['mean accuracy'].to_list()
+nesterovs_accr_list = df_lrn_alg_accr.iloc[100:200, :]['mean accuracy'].to_list()
+adam_accr_list = df_lrn_alg_accr.iloc[200:, :]['mean accuracy'].to_list()
+epochs = list(range(1, 101))
+
+plt.xlabel('epochs')
+plt.xticks([1, 25, 50, 75, 100])
+plt.ylabel('Test Accuracy')
+plt.ylim(0, 1.0)
+plt.plot(epochs, lbfgs_accr_list, label='lbfgs')
+plt.plot(epochs, nesterovs_accr_list, label='sgd')
+plt.plot(epochs, adam_accr_list, label='adam')
+plt.legend(loc='lower right')
+plt.show()
+
 
 ####################
 # RQ 3-1. learning rate(layers: 2, neurons: 64)
 # - learning algorithm : adam
 ####################
-# df_adam_accr = pd.read_csv('./results/mlp_adam_accr_2_64_iter1.csv')
-# print(df_adam_accr)
-#
-# lr_1e_1_list = df_adam_accr.iloc[:100, :]['mean accuracy'].to_list()
-# lr_1e_2_list = df_adam_accr.iloc[100:200, :]['mean accuracy'].to_list()
-# lr_1e_3_list = df_adam_accr.iloc[200:, :]['mean accuracy'].to_list()
-# epochs = list(range(1, 101))
-#
-# plt.xlabel('epochs')
-# plt.xticks([1, 25, 50, 75, 100])
-# plt.ylabel('Test Accuracy')
-# plt.ylim(0, 1.0)
-# plt.plot(epochs, lr_1e_1_list, label='lr=0.1')
-# plt.plot(epochs, lr_1e_2_list, label='lr=0.01')
-# plt.plot(epochs, lr_1e_3_list, label='lr=0.001')
-# plt.legend(loc='lower right')
-# plt.show()
+df_adam_accr = pd.read_csv('./results/mlp_adam_accr_2_64_iter1.csv')
+print(df_adam_accr)
+
+lr_1e_1_list = df_adam_accr.iloc[:100, :]['mean accuracy'].to_list()
+lr_1e_2_list = df_adam_accr.iloc[100:200, :]['mean accuracy'].to_list()
+lr_1e_3_list = df_adam_accr.iloc[200:, :]['mean accuracy'].to_list()
+epochs = list(range(1, 101))
+
+plt.xlabel('epochs')
+plt.xticks([1, 25, 50, 75, 100])
+plt.ylabel('Test Accuracy')
+plt.ylim(0, 1.0)
+plt.plot(epochs, lr_1e_1_list, label='lr=0.1')
+plt.plot(epochs, lr_1e_2_list, label='lr=0.01')
+plt.plot(epochs, lr_1e_3_list, label='lr=0.001')
+plt.legend(loc='lower right')
+plt.show()
+
 
 ####################
 # RQ 5&6. penalty parameter(C), number of support vectors
